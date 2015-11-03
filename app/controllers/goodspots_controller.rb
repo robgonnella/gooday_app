ffbf5f5976923bc8fab4f793ce519481497fa0a0
Rob Gonnella <robgonnella@gmail.com>
2015-11-02 18:41:13 -0800

    add params method to goodspots_controller.rb

--- app/controllers/goodspots_controller.rb
+++ app/controllers/goodspots_controller.rb
@@ -1,2 +1,14 @@
 class GoodspotsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+    private
+    # Implement Strong Params
+    def goodspot_params
+      params.require(:goodspot).permit(:name, :description, :location, :category
+    end
 end
+
